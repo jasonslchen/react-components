1df15b6478ee1b4ebd8d74b1711d115925599ff4
helloitjase <jason.sl.chen@gmail.com>
2019-07-29 16:19:45 -0700

    complete react component bare min

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+var Chips = () => (<li>Chips</li>);
+
+var Bacon = () => (<li>Pig fat</li>);
+// var GroceryListItem = (prop) => (
+//     <li>{prop.item}</li>
+// );
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemOver() {
+    this.setState ({
+      done: !this.state.done
+    });
+  }
+  
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.onListItemOver.bind(this)}>{this.props.item}</li>
+      );
+    }
+  }
+  
+  var GroceryList = () => (
+      <ul>
+          <GroceryListItem item={'Chocolate Covered Bacon'} />
+          <GroceryListItem item={'Chips'} />
+      </ul>
+    );
+    
+    
+    ReactDOM.render(<GroceryList/>, document.getElementById("app"));
